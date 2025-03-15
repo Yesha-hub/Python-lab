220278dd4d1564cf3d872156d89be6273256b586
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-15 21:33:38 +0530

    Create swap values.py

---
+++ swap values.py
@@ -0,0 +1,11 @@
+x=15
+y=25
+temp=x
+x=y
+y=temp
+print('The value of x after swapping: ',x)
+print('The value of y after swapping: ',y)
+
+output
+The value of x after swapping:  25
+The value of y after swapping:  15
