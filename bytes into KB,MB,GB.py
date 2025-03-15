15b8d83164a1cf027f38757a01b35dfbc9cecca3
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-15 11:53:31 +0530

    Create bytes into KB,MB,GB.py

---
+++ bytes into KB,MB,GB.py
@@ -0,0 +1,15 @@
+bytes=2
+KB=1000
+MB=1000000
+GB=1000000000
+Z=bytes/KB
+y=bytes/MB
+x=bytes/GB
+print(" bytes into KB",Z)
+print(" bytes into MB",y)
+print(" bytes into GB",x)
+
+output
+bytes into KB 0.002
+ bytes into MB 2e-06
+ bytes into GB 2e-09
