f75c21140ad04b603a86ebc7bed2b85b1447111c
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-16 11:24:32 +0530

    Create divisiblity by 7.py

---
+++ divisiblity by 7.py
@@ -0,0 +1,9 @@
+x=int(input(" enter the number "))
+if x%7==0:
+   print(" the number is divisible by 7 ")
+else:
+   print(" the number is not divisible by 7 ")
+
+output
+enter the number 49
+ the number is divisible by 7 
