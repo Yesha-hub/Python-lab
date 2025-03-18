98848a32f41fe5ee7bccb91f4f72a736bf71d726
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-18 21:37:36 +0530

    Create rectangle area vs perimeter.py

---
+++ rectangle area vs perimeter.py
@@ -0,0 +1,15 @@
+l=6
+b=9
+area=l*b
+perimeter=2*(l+b)
+print(" the area is ",area)
+print(" the perimeter is ",perimeter)
+if area>perimeter:
+    print(" area is greater ")
+else:
+    print(" perimeter is greater ") 
+
+output
+the area is  54
+ the perimeter is  30
+ area is greater 
