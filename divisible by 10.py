e7f8012afa79af3cbc3220664a36cf604e57e81a
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-18 21:14:12 +0530

    Create divisible by 10.py

---
+++ divisible by 10.py
@@ -0,0 +1,9 @@
+x=int(input(" enter the number "))
+if x%10==0:
+    print(" the number is divisible by 10 ")
+else:
+    print(" the number is not divisible by 10 ") 
+
+output
+enter the number 30
+ the number is divisible by 10 
