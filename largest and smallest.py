d03cb96bce8bca5fbe5690107d49678782f4f9af
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-18 21:03:10 +0530

    Create largest and smallest.py

---
+++ largest and smallest.py
@@ -0,0 +1,11 @@
+x=int(input(" enter the nymber for x "))
+y=int(input(" enter the nymber for y "))
+if x>y:
+   print(" the largest number is x ")
+else:
+  print(" the largest number is y ")
+
+output
+enter the nymber for x 3
+enter the nymber for y 5
+ the largest number is y 
