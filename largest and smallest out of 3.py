f6e67eebcedbb2c9436bfdaf28c8e6458f90ca42
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-18 21:09:15 +0530

    Create largest and smallest out of 3.py

---
+++ largest and smallest out of 3.py
@@ -0,0 +1,13 @@
+x=int(input(" enter the number for x "))
+y=int(input(" enter the number for y "))
+z=int(input(" enter number for z "))
+if x>y :
+    print(" the largest number is x ")
+elif z>x :
+    print(" the largest number is z ")
+
+output
+enter the number for x 6
+ enter the number for y 4
+ enter number for z 3
+ the largest number is x 
