55e5a8d4fe351bb491abf5677f14fa1e56b5d253
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-18 21:22:11 +0530

    Create count digits.py

---
+++ count digits.py
@@ -0,0 +1,10 @@
+num=int(input(" enter the number "))
+count=0
+while num!=0:
+      num//=10
+      count +=1
+print(" number of digits " +str(count))
+
+output
+enter the number 123456
+ number of digits 6
