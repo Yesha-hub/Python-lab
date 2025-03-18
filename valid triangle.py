fb5fcbb54498245e0a91afa3a4896e366c4a67b6
Yesha-hub <24BCH152@spt.pdpu.ac.in>
2025-03-18 21:29:20 +0530

    Create valid triangle.py

---
+++ valid triangle.py
@@ -0,0 +1,13 @@
+x=int(input(" enter the side x "))
+y=int(input(" enter the side y "))
+z=int(input(" enter the side z "))
+if x+y+z=180:
+   print(" it is a valid triangle ")
+else:
+   print(" it is not a valid triangle ")
+
+output
+enter the side x 22
+ enter the side y 108
+ enter the side z 51
+ it is not a valid triangle 
